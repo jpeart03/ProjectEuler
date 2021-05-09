2f1f1067940edea3e22113eea2337ef5296606b9
jpeart03 <james.r.peart@gmail.com>
2021-05-09 09:42:01 -0500

    Add evenFib

---
+++ evenFib.js
@@ -0,0 +1,19 @@
+const buildFibSeq = (max) => {
+    let fibSeq = [1, 2]
+
+    for (i = 1;
+        (fibSeq[i] + fibSeq[i - 1]) <= max; i++) {
+        fibSeq.push(fibSeq[i] + fibSeq[i - 1])
+    }
+
+    return fibSeq
+}
+
+const getSumEvenFibs = (max) => {
+    let allFibs = buildFibSeq(max)
+    let evenFibs = allFibs.filter(f => f % 2 === 0)
+
+    return evenFibs.reduce((acc, cur) => acc + cur)
+}
+
+console.dir(getSumEvenFibs(4000000))
